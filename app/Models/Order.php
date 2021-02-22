28c5ce50f12bd90fbbda2b7eec9613c632d74c61
Renzo F <renzofernandini@gmail.com>
2021-02-22 16:25:19 -0500

    did my best, with a bit of help i can totally finish it

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\PrintSheetItem;
+
 
 class Order extends Model
 {
@@ -53,10 +55,12 @@ class Order extends Model
 
     // function that checks if a product fits on a printsheet based on its size and the space available
     public static function checkFit($product) {
-
+        // get the total number of blocks in the grid that the product occupies
         $productSize = self::calculateSize($product->size);
 
-
+        // my plan here begun to fall appart
+        // i thought i would compare the number of blocks available (150 - the products already placed) against the $productSize and that would determine whether it would fit or not
+        // but this doesn't account for shapes or overlaps... so it doesn't really work at all
 
         $checked = false;
 
@@ -64,9 +68,32 @@ class Order extends Model
     }
 
     // function that places the product (item) into a printsheet
-    public static function placeItem() {
+    public static function placeItem($product) {
+        // variable to store the position of the placed item
         $position = null;
 
+        // variable to get the width of the item
+        $product_width_raw = explode('*', $product->size);
+        $product_width = $product_width_raw[0];
+
+        // variable to get the height of the item
+        $product_height_raw = explode('*', $product->size);
+        $product_height = $product_height_raw[1];
+
+        // variable to get the corresponding entry in the order_product table
+        $order_item_id = OrderItem::where('product_id', $product->orders()->first()->pivot->product_id)->where('order_id', $product->orders()->first()->pivot->order_id)->first()->id;
+
+        // create a new entry in the print_sheet_item table with the product/order details
+        $item = new PrintSheetItem();
+        $item->width = $product_width;
+        $item->height = $product_height;
+        $item->x_pos = null; // not sure how to determine the x position
+        $item->y_pos = null; // not sure how to determine the y position
+        $item->order_item_id = $order_item_id;
+        $item->ps_id = null; // need to create the printsheet before this
+        $item->save();
+
+        // return the position of the placed item
         return $position;
     }
 
@@ -85,6 +112,7 @@ class Order extends Model
                 case '*':
                     $p = $matches[1] * $matches[3];
                     break;
+
                 case '/':
                     $p = $matches[1] / $matches[3];
                     break;
